a44513e649cd3a8f53983c8f8d838caf45e4d773
Henning Hontheim <henning@hontheim.net>
2019-01-11 13:53:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java-Memory
+Displays current memory allocation
---
+++ src/main/java/net/hontheim/MainApp.java
@@ -0,0 +1,20 @@
+package net.hontheim;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(new GridPane(), 160, 90));
+        stage.show();
+    }
+}
